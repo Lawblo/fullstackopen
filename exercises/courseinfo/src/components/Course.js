0e69c6e0ef7304a4823a04b74cc11f631e17ca7f
Thorkil <schjodt.thorkil@gmail.com>
2023-01-03 13:40:53 +0100

    Completed 2.3 and 2.4

--- exercises/courseinfo/src/components/Course.js
+++ exercises/courseinfo/src/components/Course.js
@@ -1,21 +1,27 @@
-const Part = ({part}) => {
+const DisplayParts = ({parts}) => (
+    parts.map(part => <Part key={part.id} part={part} />)
+)
+
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const TotalExercises = ({ parts }) => {
+    const totalNum = parts.reduce(
+        (previous, part) => previous + (part.exercises), 0
+    )
+
     return (
-        <li>{part.name} {part.exercises}</li>
+        <p><strong>total of {totalNum} exercises</strong></p>
     )
 }
 
 const Course = ({course}) => {
     return (
-        <>
+        <div>
             <h1>{course.name}</h1>
-            <ul>
-                {course.parts.map(part =>
-                    <Part key={part.id} part={part} />
-                )}
-            </ul>
-        </>
+            <DisplayParts parts={course.parts}/>
+            <TotalExercises parts={course.parts}/>
+        </div>
     )
-
 }
 
 export default Course
