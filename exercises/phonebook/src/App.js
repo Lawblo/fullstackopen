6df76148a953165e461c987701c15404fe877ad2
Thorkil <schjodt.thorkil@gmail.com>
2023-01-07 12:41:44 +0100

    Completed 2.9

--- exercises/phonebook/src/App.js
+++ exercises/phonebook/src/App.js
@@ -9,10 +9,16 @@ const App = () => {
     {
       name: 'Arto Hellas',
       number: '040-1234567',
-    }
+      id: 1
+    },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -23,7 +29,8 @@ const App = () => {
     else {
       const personObject = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(personObject))
     }
@@ -39,6 +46,11 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(nameFilter)
+    setNewFilter(event.target.value)
+  }
+
   const isDuplicate = () => {
     const duplicateFound = persons.filter(
       person => person.name === newName
@@ -49,9 +61,13 @@ const App = () => {
     return false
   }
 
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with
+      <input onChange={handleFilterChange}/> 
+      <h3>Add a new person</h3>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -71,9 +87,14 @@ const App = () => {
           <button type='submit'>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map(person => <Person key={person.name} person={person} />)}
+        {persons
+          .filter(person => 
+            person.name
+            .toLowerCase()
+            .includes(nameFilter.toLowerCase()))
+          .map(person => <Person key={person.id} person={person} />)}
       </ul>
     </div>
   );
