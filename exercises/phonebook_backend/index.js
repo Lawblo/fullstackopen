aa5f71800b5a747cbbef963c8a8fb35ba2855c5e
Thorkil <schjodt.thorkil@gmail.com>
2023-02-26 11:46:12 +0100

    working on monogdb exercices for phonebook

--- exercises/phonebook_backend/index.js
+++ exercises/phonebook_backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
+const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
+
 const Person = require('./models/person')
 
-app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
@@ -13,31 +14,8 @@ const unknownEndpoint = (request, response, next) => {
 }
 
 morgan.token('request_data', request => JSON.stringify(request.body))
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request_data'))
 
-persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
 
 app.get('/', (request, response) => {
   response.redirect('/api/persons')
--- projects/backend/index.js
+++ projects/backend/index.js
@@ -1,74 +1,65 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
+
 const Note = require('./models/note')
 
 
-const unknownEndpoint = (request, response, next) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+
   next()
 }
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
-]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
 
 app.get('/api/notes', (request, response) => {
-  Note
-    .find({})
-    .then(notes => {
-      response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      }
+      else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  }
-  else {
-    response.status(404).end()
-  }
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findOneAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.filter(note => note.id !== id)
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-
-  return maxId
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -87,21 +78,25 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-// testing out changing importance
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
 
-  const note = notes.find(n => n.id === id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  if (!note) {
-    return response.status(400).end()
+  const note = {
+    content: body.content,
+    important: body.important,
   }
-  note.important = !note.important
 
-  return response.status(200).json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
